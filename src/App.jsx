079e31328d224ece3edb591c0867dcac3e0fdc54
Daniel Castillo <dlcastillo3015@gmail.com>
2023-02-22 10:35:10 -0500

    layout to modal component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
+import { useState } from "react";
 import Suscription from "./components/Suscription";
+import Modal from "./components/Modal";
 
 const App = () => {
+  const [showModal, setShowModal] = useState(false);
+
   return (
     <div className="flex flex-col gap-5 justify-center items-center">
+      <Modal showModal={showModal} hideModal={() => setShowModal(false)} />
+
       <button
         type="button"
         className="focus:outline-none text-white focus:ring-4 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 bg-purple-600 hover:bg-purple-700 focus:ring-purple-900"
+        onClick={() => setShowModal(true)}
       >
         Add new suscription
       </button>
