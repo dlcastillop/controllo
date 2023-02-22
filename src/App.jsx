846cc2aa1f1a3056ebd2bd1296bfdd06f45bbc73
Daniel Castillo <dlcastillo3015@gmail.com>
2023-02-22 07:37:22 -0500

    layout to add suscription button

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,14 @@
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div className="flex flex-col gap-5 justify-center items-center">
+      <button
+        type="button"
+        className="focus:outline-none text-white focus:ring-4 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 bg-purple-600 hover:bg-purple-700 focus:ring-purple-900"
+      >
+        Add new suscription
+      </button>
+    </div>
+  );
 }
 
 export default App;
