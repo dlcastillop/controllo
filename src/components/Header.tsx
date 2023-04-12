389f95d44ae795a758250df5f091e511d64252bd
Daniel Castillo <dlcastillo3015@gmail.com>
2023-04-12 09:28:00 -0400

    build: header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,17 @@
+function Header() {
+  return (
+    <header>
+      <div className="navbar bg-base-100">
+        <a
+          className="text-xl font-bold"
+          href="https://github.com/dlcastillop/controllo"
+          target="_blank">
+          Controllo
+        </a>
+      </div>
+      <hr className="h-px border-0 bg-primary" />
+    </header>
+  )
+}
+
+export default Header
--- src/popup.tsx
+++ src/popup.tsx
@@ -1,7 +1,15 @@
+import Header from "~components/Header"
+
 import "./styles.css"
 
 function IndexPopup() {
-  return <></>
+  return (
+    <div className="w-96 h-96">
+      <div className="w-5/6 m-auto">
+        <Header />
+      </div>
+    </div>
+  )
 }
 
 export default IndexPopup
