c9f3da498d65a4879dced6117e7446a0c76a256a
Daniel Castillo <dlcastillo3015@gmail.com>
2023-04-13 16:21:45 -0400

    build: add button

--- src/popup.tsx
+++ src/popup.tsx
@@ -6,9 +6,20 @@ import "./styles.css"
 const IndexPopup = () => {
   return (
     <div className="w-96 h-96">
-      <div className="w-5/6 m-auto">
-        <Header />
-        <ul className="flex flex-col items-center gap-5">
+      <div className="w-5/6 flex flex-col gap-5 items-center m-auto">
+        {/* <Header /> */}
+        <button className="btn btn-sm btn-primary rounded">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            viewBox="0 0 24 24"
+            fill="currentColor"
+            className="text-white">
+            <path d="M19 11h-6V5h-2v6H5v2h6v6h2v-6h6z"></path>
+          </svg>
+        </button>
+        <ul className="flex flex-col items-center gap-5 w-full">
           <Suscription />
         </ul>
       </div>
