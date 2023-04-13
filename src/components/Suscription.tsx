4d4861c7c0cd3f3204c68cde4fb36712a9e95585
Daniel Castillo <dlcastillo3015@gmail.com>
2023-04-13 15:37:10 -0400

    build: icon position

--- src/components/Suscription.tsx
+++ src/components/Suscription.tsx
@@ -6,14 +6,14 @@ const Suscription = () => {
           <p className="text-sm font-medium truncate">Netflix</p>
           <p className="text-sm truncate">$20/month</p>
         </div>
-        <div className="flex justify-center gap-3">
+        <div className="flex justify-center gap-3 items-center">
           <p className="text-sm font-medium">24/2/23</p>
           <div className="dropdown dropdown-hover dropdown-left">
             <label tabIndex={0}>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
+                width="16"
+                height="16"
                 viewBox="0 0 24 24">
                 <path d="M12 10c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2zm0-6c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2zm0 12c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2z"></path>
               </svg>
