e272e1f568d8d9b31d3b55aae2e42c208e54493a
Daniel Castillo <dlcastillo3015@gmail.com>
2023-04-13 16:31:56 -0400

    fix: suscription svg color

--- src/components/Suscription.tsx
+++ src/components/Suscription.tsx
@@ -14,7 +14,8 @@ const Suscription = () => {
                 xmlns="http://www.w3.org/2000/svg"
                 width="16"
                 height="16"
-                viewBox="0 0 24 24">
+                viewBox="0 0 24 24"
+                fill="currentColor">
                 <path d="M12 10c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2zm0-6c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2zm0 12c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2z"></path>
               </svg>
             </label>
