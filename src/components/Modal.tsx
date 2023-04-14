8ef36ff8eab78fd3dc98ef84ada70fc56fe8558d
Daniel Castillo <dlcastillo3015@gmail.com>
2023-04-14 10:43:11 -0400

    feat: check inputs in modal component

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,4 +1,42 @@
 const Modal = ({ title, action }) => {
+  const checkInput = (value: String | Number, label: HTMLInputElement) => {
+    if (value === "") {
+      label.classList.remove("hidden")
+      label.classList.add("block")
+      return false
+    } else {
+      label.classList.remove("block")
+      label.classList.add("hidden")
+      return true
+    }
+  }
+
+  const handleClick = () => {
+    const service = (
+      document.querySelector("#service-input") as HTMLInputElement
+    ).value
+    const amount = (document.querySelector("#amount-input") as HTMLInputElement)
+      .value
+    const date = (document.querySelector("#date-input") as HTMLInputElement)
+      .value
+
+    const $serviceLabel = document.querySelector(
+      "#service-label"
+    ) as HTMLInputElement
+    const $amountLabel = document.querySelector(
+      "#amount-label"
+    ) as HTMLInputElement
+    const $dateLabel = document.querySelector("#date-label") as HTMLInputElement
+
+    const isService = checkInput(service, $serviceLabel)
+    const isAmount = checkInput(amount, $amountLabel)
+    const isDate = checkInput(date, $dateLabel)
+
+    if (isService && isAmount && isDate) {
+      //Hacer algo
+    }
+  }
+
   return (
     <>
       <input type="checkbox" id="modal" className="modal-toggle visible" />
@@ -11,9 +49,12 @@ const Modal = ({ title, action }) => {
                 type="text"
                 placeholder="What is the service name?"
                 className="input input-bordered input-primary w-full max-w-xs bg-neutral-focus text-neutral-content"
+                id="service-input"
               />
-              <label className="label hidden">
-                <span className="label-text-alt">Error</span>
+              <label className="label hidden p-0.5" id="service-label">
+                <span className="label-text-alt text-red-600">
+                  Introduce the service name
+                </span>
               </label>
             </div>
 
@@ -23,9 +64,12 @@ const Modal = ({ title, action }) => {
                 placeholder="How much you pay?"
                 className="input input-bordered input-primary w-full max-w-xs bg-neutral-focus text-neutral-content"
                 min={0}
+                id="amount-input"
               />
-              <label className="label hidden">
-                <span className="label-text-alt">Error</span>
+              <label className="label hidden p-0.5" id="amount-label">
+                <span className="label-text-alt text-red-600">
+                  Introduce how much you pay
+                </span>
               </label>
             </div>
 
@@ -41,14 +85,17 @@ const Modal = ({ title, action }) => {
                 type="date"
                 className="input input-bordered input-primary w-full max-w-xs bg-neutral-focus text-neutral-content"
                 min={0}
+                id="date-input"
               />
-              <label className="label hidden">
-                <span className="label-text-alt">Error</span>
+              <label className="label hidden p-0.5" id="date-label">
+                <span className="label-text-alt text-red-600">
+                  Introduce when is the next payment
+                </span>
               </label>
             </div>
           </div>
           <div className="modal-action justify-center">
-            <label htmlFor="modal" className="btn btn-primary rounded">
+            <label className="btn btn-primary rounded" onClick={handleClick}>
               {action}
             </label>
           </div>
